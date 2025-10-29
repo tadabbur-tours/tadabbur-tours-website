5dc261b1112500fca645a2a3b3ee84ab83d3f128
jawad012 <jawad012@umn.edu>
2025-10-28 19:33:17 -0500

    Simplify ESLint config and disable completely in Next.js config for Vercel deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,24 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "react/no-unescaped-entities": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "off",
-    },
-  },
+  ...compat.extends("next/core-web-vitals"),
 ];
 
 export default eslintConfig;
